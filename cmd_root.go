5e18cca4d725085bd9f6d642887ee3192653ca39
yasukotelin <yasukotelin@gmail.com>
2019-02-11 14:36:00 +0900

    refactor: tmpファイル操作系を構造体+メソッド化

--- cmd_root.go
+++ cmd_root.go
@@ -2,25 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"runtime"
-	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/yasukotelin/feditelin/tmpfile"
 )
 
 const (
-	version     = "0.1.0"
-	tmpFileName = "tmp-file-list.txt"
+	version = "0.1.0"
 )
 
 var (
-	editor      string
-	defEditor   string
-	newLineCode string
+	argEditor string
 
 	rootCmd = &cobra.Command{
 		Use:   "feditelin",
@@ -31,18 +24,10 @@ var (
 )
 
 func init() {
-	if runtime.GOOS == "windows" {
-		defEditor = "notepad"
-		newLineCode = "\r\n"
-	} else {
-		defEditor = "vim"
-		newLineCode = "\n"
-	}
-
 	rootCmd.Version = version
 
 	// Flag定義
-	rootCmd.PersistentFlags().StringVarP(&editor, "editor", "e", defEditor, "specify the editor to open. ")
+	rootCmd.PersistentFlags().StringVarP(&argEditor, "editor", "e", "", "specify the editor to open. ")
 }
 
 func run(cmd *cobra.Command, args []string) {
@@ -51,122 +36,26 @@ func run(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
-	tmpFilePath, err := createTmpFile(args[0])
-	if err != nil {
+	tmpFile := tmpfile.NewTmpFile(args[0])
+	if err := tmpFile.Create(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	if err := openEditor(editor, tmpFilePath); err != nil {
+	if err := tmpFile.OpenWithEditor(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// 後始末（tmpファイルの削除）
-	err = os.Remove(tmpFilePath)
-	if err != nil {
+	if err := tmpFile.Delete(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-// Execute root command.
 func execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func createTmpFile(dir string) (tmpFilePath string, err error) {
-	exeDir, err := getExeDirPath()
-	if err != nil {
-		return tmpFileName, err
-	}
-
-	tmpFilePath = filepath.Join(exeDir, tmpFileName)
-
-	file, err := os.Create(tmpFilePath)
-	if err != nil {
-		return tmpFileName, err
-	}
-	defer file.Close()
-
-	// 引数Directoryからファイル一覧読み込み
-	fileProps, err := readFiles(dir)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	file.WriteString(makeTmpFileDescriMsg())
-
-	for _, p := range fileProps {
-		_, err := file.WriteString(p.Name + newLineCode)
-		if err != nil {
-			return tmpFileName, err
-		}
-	}
-
-	return tmpFileName, nil
-}
-
-// OpenEditor 指定editorでfPathを実行する
-func openEditor(editor string, fPath string) error {
-	execCmd := exec.Command(editor, fPath)
-	if err := execCmd.Run(); err != nil {
-		return err
-	}
-	return nil
-}
-
-// GetExeDirPath アプリ実行ディレクトリのパスを返却する
-func getExeDirPath() (string, error) {
-	p, err := os.Executable()
-	if err != nil {
-		return "", err
-	}
-	return filepath.Dir(p), nil
-}
-
-// ReadFiles ディレクトリからファイル一覧を返却する
-func readFiles(dir string) ([]fileRowProp, error) {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	var res []fileRowProp
-	index := 0
-	for _, f := range files {
-		if !f.IsDir() {
-			row := fileRowProp{
-				Index:   index,
-				Path:    filepath.Join(dir, f.Name()),
-				Name:    f.Name(),
-				NewName: "",
-			}
-			res = append(res, row)
-			index++
-		}
-	}
-	return res, nil
-}
-
-func makeTmpFileDescriMsg() string {
-	tmpFileDescriMsg := []string{
-		"// リネームしたいファイル名を編集してください。",
-		"// ファイル名の編集が完了したら保存して閉じてください。",
-		"// 編集したファイル名へのリネーム処理が実行されます。",
-		"",
-		"// ファイルのリネーム判定は表示されている既定の行に対して行われます。",
-		"// 改行や行削除などを実施し行をずらした場合には、",
-		"// ファイル名が一様に変更されてしまうため注意してください。",
-		"",
-		"// 編集が完了し閉じた後、",
-		"// 最終確認が表示されるのでキャンセルしたい場合は一度保存して次へ進めてください。",
-		"",
-		"// ----- files ------",
-	}
-	return strings.Join(tmpFileDescriMsg, newLineCode) + newLineCode
 }
--- types.go
+++ types.go
@@ -1,9 +1,1 @@
 package main
-
-// fileRowProp tmpファイル1行の情報を保持する構造体
-type fileRowProp struct {
-	Index   int
-	Path    string
-	Name    string
-	NewName string
-}
